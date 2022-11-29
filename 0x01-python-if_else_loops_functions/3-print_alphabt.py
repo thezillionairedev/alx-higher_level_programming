93c51568644719a1fb92b4eb81e27d3aa85b1654
thezillionairedev <thezillionairedev@gmail.com>
2022-11-29 04:03:15 -0800

    the ASCII alphabet in lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord("a"), ord("z") + 1):
-    if chr(i) != "e" and char(i) != "q":
-        print("{:c}".format(i), end="")
+for i range(97, 123):
+    if chr(i) is not 'q' and chr(i) is not 'e':
+        print("{}".format(chr(i)), end="")
