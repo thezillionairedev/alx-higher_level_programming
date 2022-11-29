8b02949a2bdd4ca10dfb2f4faa6510fa0657a492
thezillionairedev <thezillionairedev@gmail.com>
2022-11-29 04:23:08 -0800

    updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) is not 'q' and chr(i) is not 'e':
-        print("{:s}".format(chr(i)), end="")
+for i in 'abcdfghijklmnoprstuvwxyz':
+    print("{:s}".format(i), end="")
